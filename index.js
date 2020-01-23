34c332ac185c73e35000ace3290d280ab3fb8045
Nuno Pereira <hello@nunopereiradev.com>
2020-01-23 15:29:26 +0000

    Removed Unwanted Routes

--- index.js
+++ index.js
@@ -25,10 +25,8 @@ app.use(cors({ origin: 'http://localhost:3000' }))
 
 // ! ROUTES
 const authRoute = require('./routes/Auth')
-const postRoute = require('./routes/Posts')
 
 // ! ROUTE MIDDLEWARES
 app.use('/auth', authRoute)
-app.use('/', postRoute)
 
 app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`))
--- routes/posts.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const verify = require('../utils/protectedRoutes')
-
-router.get('/', verify, (req, res) => {
-  res.json({ posts: { title: 'My first post', description: 'Private Data' } })
-})
-
-module.exports = router
